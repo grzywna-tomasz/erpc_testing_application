a986e4c28526512ef8f4a772fcdd488b89d55057
grzywna-tomasz <grzywna997@gmail.com>
2026-05-02 10:32:34 +0200

    Add interface for reading and writing memory. We can read and write variables by name - we need list of variables available in elf.

--- client.cpp
+++ client.cpp
@@ -10,6 +10,10 @@
 #include <sstream>
 #include <iomanip>
 #include "erpc_port.h"
+#include <fstream>
+#include "nlohmann/json.hpp"
+#include <optional>
+#include <sstream>
 
 template< typename T >
 std::string int_to_hex( T i )
@@ -17,6 +21,43 @@ std::string int_to_hex( T i )
   std::stringstream stream;
   stream << "0x" << std::setfill ('0') << std::setw(sizeof(T)*2) << std::hex << i;
   return stream.str();
+}
+
+int nibbleStrToInt(char c)
+{
+    int ret_val;
+    if ((c >= 'a') && (c <= 'f'))
+    {
+        ret_val = c - 'a' + 10;
+    }
+    else if ((c >= '0') && (c <= '9'))
+    {
+        ret_val = c - '0';
+    }
+    else if ((c >= 'A') && (c <= 'F'))
+    {
+        ret_val = c - 'A' + 10;
+    }
+    return ret_val;
+}
+
+std::vector<uint8_t> hexToBytes(const std::string& hex)
+{
+    int start_index = 0;
+    if (hex.compare(0, 2, "0x") == 0)
+    {
+        start_index = 2;
+    }
+
+    /* Divide by 2 to get number of bytes, not nibbles */
+    std::vector<uint8_t> ret_val((hex.length() - start_index) / 2);
+
+    for (int index = start_index; index < hex.length(); index += 2)
+    {
+        ret_val[(index - start_index) / 2] = nibbleStrToInt(hex[index]) * 16 + nibbleStrToInt(hex[index + 1]);
+    }
+
+    return ret_val;
 }
 
 constexpr int ADDITIONAL_DATA_0X200_SIZE {3};
@@ -39,6 +80,12 @@ public:
         }
     }
 };
+
+typedef struct
+{
+    uint32_t address;
+    uint32_t size;
+} VariableData_t;
 
 class ErrorPrinter
 {
@@ -84,11 +131,55 @@ private:
     }
 };
 
+class JsonVariableReader
+{
+public:
+    JsonVariableReader(const std::string& path_to_file)
+    {
+        try 
+        {
+            std::ifstream file(path_to_file);
+            m_json = nlohmann::json::parse(file);
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Error: " << e.what() << "\n";
+        }
+    }
+
+    std::optional<VariableData_t> getVariableData(const std::string& var_name) const
+    {
+        if (!m_json.contains(var_name))
+        {
+            return std::nullopt;
+        }
+        else
+        {
+            return VariableData_t {.address = m_json[var_name]["addr"], .size = m_json[var_name]["size"]};
+        }
+    }
+
+    std::optional<int> getVariableSize(const std::string& var_name) const
+    {
+        if (!m_json.contains(var_name))
+        {
+            return std::nullopt;
+        }
+        else
+        {
+            return int {m_json[var_name]["size"]};
+        }
+    }
+
+private:
+    nlohmann::json m_json {};
+};
+
 class ApplicationClient
 {
 public:
-    ApplicationClient(erpcShim::Communication_client *communication_client) : 
-        m_communication_client {communication_client}
+    ApplicationClient(erpcShim::Communication_client *communication_client, const std::string& path_to_file) : 
+        m_communication_client {communication_client}, m_variable_reader {path_to_file}
     {
 
     }
@@ -115,6 +206,92 @@ public:
             std::cout << "Failed to receive Errors_GetAll\n";
         }
 
+        free(errors);
+    }
+
+    void ClearErrors()
+    {
+        std::cout << "Clear errors status: " << print(m_communication_client->Errors_ClearAll());
+    }
+
+    void Debug_ReadVariableFromRam(const std::string& var_name)
+    {
+        std::optional<VariableData_t> variable {m_variable_reader.getVariableData(var_name)};
+        if (variable)
+        {
+            list_uint8_1_t *variable_value = (list_uint8_1_t *)erpc_malloc(sizeof(list_uint8_1_t));
+            if (ERPC_OK == m_communication_client->Memory_Read(variable.value().address, variable.value().size, variable_value))
+            {
+                if (variable_value)
+                {
+                    std::cout << var_name << " has value 0x" << std::hex << std::setfill('0');
+                    for (int i = 0; i < variable_value->elementsCount; ++i)
+                    {
+                        // std::cout << std::format("{:02x}", variable_value->elements[i]);
+                        std::cout << std::setw(2) << static_cast<int>(variable_value->elements[i]);
+                    }
+                    std::cout << std::dec << std::setfill(' ') << "\n";
+                }
+            }
+            else
+            {
+                std::cout << "Reading variable from RAM failed\n";
+            }
+            free(variable_value);
+        }
+        else
+        {
+            std::cout << "Variable: " << var_name << "not in json\n";
+        }
+    }
+
+    void Debug_WriteVariableToRam(const std::string& var_name, uint8_t *data, uint32_t data_size)
+    {
+        std::optional<VariableData_t> variable {m_variable_reader.getVariableData(var_name)};
+        if (variable)
+        {
+            if (data_size == variable.value().size)
+            {
+                const list_uint8_1_t data_to_send {.elements = data, .elementsCount = data_size};
+                if (ERPC_OK == m_communication_client->Memory_Write(variable.value().address, &data_to_send))
+                {
+                    std::cout << var_name << " written successfully\n";
+                }
+                else
+                {
+                    std::cout << "failed to write " << var_name << "\n";
+                }
+            }
+            else
+            {
+                std::cout << "Failed to write variable " << var_name << "size do not match (expected " << variable.value().size << ", given " << data_size << ")\n";
+            }
+        }
+        else
+        {
+            std::cout << "Variable: " << var_name << "not in json\n";
+        }
+    }
+
+private:
+    erpcShim::Communication_client *m_communication_client;
+    ErrorPrinter m_error_printer;
+    JsonVariableReader m_variable_reader;
+
+    std::string_view print(Erpc_Status_t status)
+    {
+        if (status == ERPC_OK)
+        {
+            return "Operation successful\n";
+        }
+        else
+        {
+            return "Operation failed\n";
+        }
+    }
+
+    void free(list_Erpc_ErrorInfo_t_1_t *errors)
+    {
         if (errors)
         {
             if (errors->elements)
@@ -132,25 +309,15 @@ public:
         }
     }
 
-    void ClearErrors()
+    void free(list_uint8_1_t *list)
     {
-        std::cout << "Clear errors status: " << print(m_communication_client->Errors_ClearAll());
-    }
-
-
-private:
-    erpcShim::Communication_client *m_communication_client;
-    ErrorPrinter m_error_printer;
-
-    std::string_view print(Erpc_Status_t status)
-    {
-        if (status == ERPC_OK)
+        if (list)
         {
-            return "Operation successful\n";
-        }
-        else
-        {
-            return "Operation failed\n";
+            if (list->elements)
+            {
+                erpc_free(list->elements);
+            }
+            erpc_free(list);
         }
     }
 };
@@ -174,38 +341,64 @@ int main()
     clientManager.setTransport(&transport);
     clientManager.setCodecFactory(&codecFactory);
 
-    ApplicationClient appClient {new erpcShim::Communication_client(&clientManager)};
+    ApplicationClient appClient {new erpcShim::Communication_client(&clientManager), "../../../Nucleo_F439ZI/build/Debug/variables.json"};
     // erpcShim::Communication_client client(&clientManager);
 
     while(1)
     {
         std::cout << "Request action: ";
 
-        std::string action;
-        std::cin >> action;
-        if (action == "exit")
+        std::string line;
+        // std::cin >> action;
+        std::getline(std::cin, line);
+        std::istringstream iss(line);
+        std::vector<std::string> args;
+        std::string word;
+        while (iss >> word)
+        {
+            args.push_back(word);
+        }
+
+
+        if (args.at(0) == "exit")
         {
             break;
         }
-        else if (action == "help")
+        else if (args.at(0) == "help")
         {
             std::cout << "Available actions:\n\
             version - get application version\n\
             errors - get all errors\n\
             clear - clear all errors\n\
+            read XXX - read variable from RAM, XXX is variable name\n\
+            write XXX 0xYYY - write data to RAM at address XXX with 0xYYY data\n\
             exit - exit application\n";
         }
-        else if (action == "version")
+        else if (args.at(0) == "version")
         {
             appClient.AppVersion_Get();
         }
-        else if (action == "errors")
+        else if (args.at(0) == "errors")
         {
             appClient.Errors_GetAll();
         }
-        else if (action == "clear")
+        else if (args.at(0) == "clear")
         {
             appClient.ClearErrors();
+        }
+        else if (args.at(0) == "read")
+        {
+            appClient.Debug_ReadVariableFromRam(args.at(1));
+        }
+        else if (args.at(0) == "write")
+        {
+            std::vector<uint8_t> data = hexToBytes(args.at(2));
+            for (int i = 0; i < 10; i++)
+            {
+                std::cout << static_cast<int>(data.data()[i]);
+            }
+            std::cout << "\n";
+            appClient.Debug_WriteVariableToRam(args.at(1), data.data(), data.size());
         }
         else
         {
